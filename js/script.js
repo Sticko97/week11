f3c95252c96d202cacdc52432132ee586c5b7853
Sticko97 <sle23@my.bcit.ca>
2022-11-15 14:54:35 -0800

    greet function

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// function greet(user){
+//     console.log(`Hello ${user}`);
+// }
+
+// greet("Stanley");
+
+// const name = function(user){
+//     console.log(`Hello ${user}`);
+//      console.log('Hello ' + user); 
+// }  
+
+// const greet = (user) =>
+// {
+//     console.log(`Hello ${user}`);
+// }
+
+// greet();
+
+function greet(user, time = "night")
+{
+    console.log(`Good ${time} ${user}`);
+}
+
+greet("Stanley");
